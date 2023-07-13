0d5a2e5e5b66bef4e5ab147cb39c0448c0437024
Bakare32 <bakarewariz012@gmail.com>
2023-07-13 12:26:20 +0100

    set up the font.

---
+++ CowryCalculatorTest/App/Presentation/View/CalculatorLayout.swift
@@ -0,0 +1,8 @@
+//
+//  CalculatorLayout.swift
+//  CowryCalculatorTest
+//
+//  Created by Bakare Waris on 13/07/2023.
+//
+
+import UIKit
---
+++ CowryCalculatorTest/App/Presentation/View/CalculatorViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CalculatorViewController.swift
+//  CowryCalculatorTest
+//
+//  Created by Bakare Waris on 13/07/2023.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
